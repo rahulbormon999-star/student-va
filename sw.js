796496410c736074659d06453bffae851858f8fd
rahulbormon999-star <rahulbormon999@gmail.com>
2026-06-09 12:18:19 +0600

    Add service worker for file caching
    Implement service worker for caching files and handling fetch events.

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "study-vault-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./nodes.html",
+  "./pdf.html",
+  "./qbp.html",
+  "./fcp.html",
+  "./exp.html",
+  "./routine.html",
+  "./logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
